a755abb8d233fb788f6b259c67e785d8239339c3
Shankar Lohar <shankarloharmail@gmail.com>
2022-03-06 14:19:54 +0530

    Login Signup with Firebase added with User database

--- app/src/main/java/com/shankarlohar/parchi/LoginActivity.kt
+++ app/src/main/java/com/shankarlohar/parchi/LoginActivity.kt
@@ -3,29 +3,82 @@ package com.shankarlohar.parchi
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 import com.shankarlohar.parchi.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var alert: AlertDialog
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.textViewForgotLoginPassword.setOnClickListener { onForgotPassword() }
+        supportActionBar?.hide()
+
+        alert = AlertDialog.Builder(this).create()
+        alert.setTitle("Login")
+        alert.setMessage("Please wait while we login you to Parchi...")
+        alert.setCanceledOnTouchOutside(false)
+
+        auth = FirebaseAuth.getInstance()
+        checkUser()
+
+        binding.textViewCreateAccount.setOnClickListener { onForgotPassword() }
         binding.buttonLogin.setOnClickListener { onLogin() }
 
     }
 
+    private fun checkUser() {
+        val firebaseUser = auth.currentUser
+        if (firebaseUser!=null){
+            startActivity(Intent(this, BottomNavActivity::class.java))
+            finish()
+        }
+    }
+
     private fun onLogin() {
-        val loginActivityIntent = Intent(this, BottomNavActivity::class.java)
-        startActivity(loginActivityIntent)
+        if (validateUser()){
+            firebaseLogin()
+        }
+    }
+
+    private fun firebaseLogin() {
+        alert.show()
+        auth.signInWithEmailAndPassword(binding.editTextUserEmail.text.toString().trim(),binding.editTextPassword.text.toString().trim())
+            .addOnSuccessListener {
+                alert.dismiss()
+                val firebaseUser = auth.currentUser
+                val userEmail = firebaseUser!!.email
+                Toast.makeText(this,"Welcome user $userEmail!",Toast.LENGTH_LONG).show()
+                startActivity(Intent(this,BottomNavActivity::class.java))
+                finish()
+            }.addOnFailureListener{
+                alert.dismiss()
+                Toast.makeText(this,"Login Failed due to ${it.message}", Toast.LENGTH_LONG).show()
+            }
+    }
+
+    private fun validateUser(): Boolean {
+        if (!Patterns.EMAIL_ADDRESS.matcher(binding.editTextUserEmail.text.toString().trim()).matches()){
+            binding.editTextUserEmail.error = "Invalid Email Format."
+        } else if (TextUtils.isEmpty(binding.editTextPassword.text.toString().trim()) || binding.editTextPassword.text.toString().trim().trim().length < 6){
+            binding.editTextPassword.error = "Enter a valid 8 character password."
+        }else {
+            return true
+        }
+        return false
     }
 
     private fun onForgotPassword(){
-        val signupActivityIntent = Intent(this, SignupActivity::class.java)
-        startActivity(signupActivityIntent)
+        startActivity(Intent(this, SignupActivity::class.java))
     }
 
 }
--- app/src/main/java/com/shankarlohar/parchi/SignupActivity.kt
+++ app/src/main/java/com/shankarlohar/parchi/SignupActivity.kt
@@ -1,11 +1,87 @@
 package com.shankarlohar.parchi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.shankarlohar.parchi.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var alert: AlertDialog
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().getReference("Users")
+        binding.buttonCreateAccount.setOnClickListener { createUserAccount() }
+    }
+
+    private fun createUserAccount() {
+        alert = AlertDialog.Builder(this).create()
+        if (validate()){
+            alert.setTitle("Registration")
+            alert.setMessage("Please wait while we register you to Parchi...")
+            alert.setCanceledOnTouchOutside(false)
+            alert.show()
+            saveUserData()
+            firebaseSignup()
+        }
+    }
+
+    private fun saveUserData() {
+        val user = Users(binding.editTextName.text.toString().trim(),binding.editTextEmail.text.toString().trim(),binding.editTextPhone.text.toString().trim(),binding.editTextSetConfirmPassword.text.toString().trim())
+        database.child(binding.editTextUserName.text.toString().trim()).setValue(user).addOnSuccessListener {
+            Toast.makeText(this,"Database Ready for user ${binding.editTextUserName.text.toString().trim()}!",Toast.LENGTH_LONG).show()
+        }.addOnFailureListener{
+            Toast.makeText(this,"Database Creation Failed due to ${it.message}",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun firebaseSignup() {
+        auth.createUserWithEmailAndPassword(binding.editTextEmail.text.toString().trim(),binding.editTextSetConfirmPassword.text.toString().trim())
+            .addOnSuccessListener {
+                alert.dismiss()
+                val firebaseUser = auth.currentUser
+                val userEmail = firebaseUser!!.email
+                Toast.makeText(this,"Account Created for user $userEmail!",Toast.LENGTH_LONG).show()
+                startActivity(Intent(this,LoginActivity::class.java))
+                finish()
+
+        }.addOnFailureListener{
+                alert.dismiss()
+                Toast.makeText(this,"Sign up Failed due to ${it.message}",Toast.LENGTH_LONG).show()
+
+            }
+    }
+
+    private fun validate(): Boolean {
+        if (!Patterns.EMAIL_ADDRESS.matcher(binding.editTextEmail.text.toString().trim()).matches()){
+            binding.editTextEmail.error = "Invalid Email Format."
+        } else if (TextUtils.isEmpty(binding.editTextSetPassword.text.toString().trim()) || binding.editTextSetPassword.text.toString().trim().length < 6){
+            binding.editTextSetPassword.error = "Enter a valid 8 character password."
+        }else if (binding.editTextSetPassword.text.toString().trim() != binding.editTextSetConfirmPassword.text.toString().trim()){
+            binding.editTextSetConfirmPassword.error = "Passwords didn't match."
+        }else {
+            return true
+        }
+        return false
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
