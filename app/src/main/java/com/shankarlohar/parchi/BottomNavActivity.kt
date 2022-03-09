f518767400fc840e625d366c705956dfbe6d7484
Shankar Lohar <shankarloharmail@gmail.com>
2022-03-09 14:48:00 +0530

    bottom nav and left drawer both added

--- app/src/main/java/com/shankarlohar/parchi/BottomNavActivity.kt
+++ app/src/main/java/com/shankarlohar/parchi/BottomNavActivity.kt
@@ -7,16 +7,16 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.commit
+
 import com.google.firebase.auth.FirebaseAuth
 import com.shankarlohar.parchi.databinding.ActivityBottomNavBinding
+import com.shankarlohar.parchi.fragments.BookFragment
+import com.shankarlohar.parchi.fragments.MyTicketsFragment
+import com.shankarlohar.parchi.fragments.SearchFragment
 
-class BottomNavActivity : AppCompatActivity() {
+class BottomNavActivity : AppCompatActivity(){
 
     lateinit var toggle: ActionBarDrawerToggle
     private lateinit var alert: AlertDialog
@@ -26,52 +26,50 @@ class BottomNavActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.title = getString(R.string.app_title)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
 
         binding = ActivityBottomNavBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportActionBar?.title = getString(R.string.app_title)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_bottom_nav)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_search, R.id.navigation_book, R.id.navigation_my_tickets))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        binding.navView.setOnItemSelectedListener{
+            when (it.itemId) {
+                R.id.search_museums -> onSearch()
+                R.id.book_ticket -> onBook()
+                R.id.booked_ticket -> onBooked()
+                else -> false
+            }
+        }
 
         toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.open, R.string.close)
         binding.drawer.addDrawerListener(toggle)
         toggle.syncState()
 
-        supportActionBar?.setDisplayShowHomeEnabled(true)
-
-        binding.drawerView.setNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.visit_history -> Toast.makeText(
-                    this,
-                    "Visit history coming soon...",
-                    Toast.LENGTH_LONG
-                ).show()
+        binding.drawerView.setNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.visit_history -> {
+                    Toast.makeText(this, "Visit history coming soon...", Toast.LENGTH_LONG).show()
+                    true
+                }
                 R.id.logout -> logoutFirebase()
                 R.id.githubrepo -> redirectToGithubRepo()
+                else -> false
             }
-            true
         }
-    }
-
+}
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)) return true
         return super.onOptionsItemSelected(item)
     }
 
-    private fun redirectToGithubRepo() {
+    private fun redirectToGithubRepo(): Boolean {
         val openURL = Intent(Intent.ACTION_VIEW)
         openURL.data = Uri.parse("https://github.com/ShankarLohar/Parchi")
         startActivity(openURL)
+        return true
     }
 
-    private fun logoutFirebase() {
+    private fun logoutFirebase(): Boolean {
         alert = AlertDialog.Builder(this).create()
         alert.setTitle("Logout")
         alert.setMessage("Please wait while we log you out from Parchi...")
@@ -80,6 +78,31 @@ class BottomNavActivity : AppCompatActivity() {
         alert.show()
         auth.signOut()
         startActivity(Intent(this, LoginActivity::class.java))
+        return true
+    }
+
+    private fun onBooked(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.nav_host_fragment_activity_bottom_nav, MyTicketsFragment())
+        }
+        return true
+    }
+
+    private fun onBook(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.nav_host_fragment_activity_bottom_nav, BookFragment())
+        }
+        return true
+    }
+
+    private fun onSearch(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.nav_host_fragment_activity_bottom_nav, SearchFragment())
+        }
+        return true
     }
 
 }
+
+
+
--- app/src/main/java/com/shankarlohar/parchi/fragments/MyTicketsFragment.kt
+++ app/src/main/java/com/shankarlohar/parchi/fragments/MyTicketsFragment.kt
@@ -1,4 +1,23 @@
 package com.shankarlohar.parchi.fragments
 
-class MyTicketsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.shankarlohar.parchi.databinding.FragmentTicketsBinding
+
+class MyTicketsFragment : Fragment() {
+
+    private lateinit var binding : FragmentTicketsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentTicketsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
 }
